d6334593703e57ddb8dbb5bcbcb308bb91f088e2
AndersonAndrad <anderson_andrade_@outlook.com>
2023-03-08 00:25:58 -0300

    config: add swagger in project
    add dependence and configure swagger in project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Cadastro beneficiario')
+    .setDescription('')
+    .setVersion('0.1')
+    .addTag('CadastroBeneficiario')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api-doc', app, swaggerDocument);
+
   await app.listen(3000);
 }
 bootstrap();
